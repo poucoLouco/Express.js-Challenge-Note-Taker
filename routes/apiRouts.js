829eb6daaa441bed960331d27259d5668e39ebce
rock-soul-ana <118241284+poucoLouco@users.noreply.github.com>
2023-03-04 19:16:49 -0800

    delete

--- routes/apiRouts.js
+++ routes/apiRouts.js
@@ -12,39 +12,38 @@ router.get('/notes', function(req, res) {
 
   
   router.post("/notes", function (req, res) {
-    // /// Pto save on the request body
-    var newNote = req.body;
-    // Add unique id to each note
-    newNote.id = notes.length + 1;
-    var updatedNotesArray = notes;
-    updatedNotesArray.push(newNote);
-    // add it to the db.json file, and then return the new note to
-    // the client. You'll need to find a way to give each note a unique
-    // id when it's saved 
-    fs.writeFile("./db/db.json", JSON.stringify({ notes: updatedNotesArray }), function () {
-      res.json(notes);
-    });
+  // /// Pto save on the request body
+  var newNote = req.body;
+  // Add unique id to each note
+  newNote.id = notes.length + 1;
+  var updatedNotesArray = notes;
+  updatedNotesArray.push(newNote);
+  // add it to the db.json file, and then return the new note to
+  // the client. You'll need to find a way to give each note a unique
+  // id when it's saved 
+  fs.writeFile("./db/db.json", JSON.stringify({ notes: updatedNotesArray }), function () {
+    res.json(notes);
   });
+});
   //DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
  // In order to delete a note,
 //you'll need to read all notes from the db.json file(fs.readFileSync("db/db.json")), 
 //and then rewrite the notes to the db.json file.
 //DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete
- router.delete("/notes/:id",(req, res) => {
+ router.delete("/api/notes/:id", function (req, res) {
+  res.send('Got a DELETE request at /user')
     var id = req.params.id;
-    readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-    // Make a new array of all tips except the one with the ID provided in the URL
-    const result = json.filter((notes) => notes.id !== id);
-
-    // Save that array to the filesystem
-    writeToFile('./db/db.json', result);
-
-    // Respond to the DELETE request
-    res.json(`Item ${notes} has been deleted 🗑️`);
+    // remove the note with the given id property,
+    db.splice(id - 1, 1);
+    // reassign id
+    db.forEach((obj, i) => {
+      obj.id = i + 1;
+    });
+    // and then rewrite the notes to the db.json file.
+    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+      res.json(db);
+    });
   });
-});
 
   module.exports = router;
 
