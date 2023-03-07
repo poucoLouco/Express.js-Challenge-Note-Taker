d422387e1b8761f48d310a0d35fe3c08966b1f21
rock-soul-ana <118241284+poucoLouco@users.noreply.github.com>
2023-03-07 11:07:31 -0800

    still with port

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ const apiRoutes = require('./routes/apiRouts');
 
 
 const htmlRoutes = require('./routes/htmlRoutes');
-const { application } = require('express');
 
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("super-duper application on port 5000"))
+const port = process.env.PORT || 3001;
 
 const app = express();
 
