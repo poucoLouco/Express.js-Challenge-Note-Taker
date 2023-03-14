fb6c41cfb8b4107f90065a6d9cf37a48f5a43509
rock-soul-ana <118241284+poucoLouco@users.noreply.github.com>
2023-03-14 16:46:49 -0700

    changet const port to PORT

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const apiRoutes = require('./routes/apiRouts');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
